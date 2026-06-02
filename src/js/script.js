ab0bacf77f3ea717b14f177ed0cb4d392b7fa8de
takomaru2 <col.dd93@gmail.com>
2026-06-02 14:01:39 +0900

    feat: FAQアコーディオンの開閉処理を複数でもできるように実装

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,3 @@
-// 質問取得  // 回答取得
-
-// クリック検知。質問がクリックされたら
-
-// 回答にopenを付けたり消したりする
-
-//要素取得はdocument.querySelector()
-// イベントはaddEventListener()
-// クラス操作はclassList
-// ===========================================================
-
-// question.addEventListener("click", () => {
-//   answer.classList.add("open");
-// });
-
-// クリックしたら戻すようにしなきゃ、でもそりゃ分岐しないとクリックした時どっち使うのってなるか
-// このまま書いてても閉じるボタン専用になるてかremoveイベントは違うそう
-// question.removeEventListener("click", () => {
-//   answer.classList.remove("open");
-// });
-
-// includesみたいな感じのやつないかなと調べてcontains出てきた
-// question.addEventListener("click", () => {
-//   if (answer.classList.contains("open")) {
-//     answer.classList.remove("open");
-//   } else {
-//     answer.classList.add("open");
-//   }
-// });
 // ==========================================
 // 質問取得  // 回答取得
 const answer = document.querySelector(".answer-wrapper");
@@ -34,15 +5,35 @@ const question = document.querySelector(".question-wrapper");
 const plus = document.querySelector(".question-img");
 const minus = document.querySelector(".minus-icon-img");
 
-// const answer2 = document.querySelector(".answer-wrapper-2");
-// const question2 = document.querySelector(".question-wrapper-2");
+const answerAll = document.querySelectorAll(".answer-wrapper");
+const questions = document.querySelectorAll(".question-wrapper");
+const plusAll = document.querySelectorAll(".question-img");
+const minusuAll = document.querySelectorAll(".minus-icon-img");
 
-question.addEventListener("click", () => {
-  answer.classList.toggle("open");
+// 一個のパターン
+// question.addEventListener("click", () => {
+//   // answer.classList.toggle("open");
+//
+//   //   ========all
+//
+//   //   画像も変更したい
+//   plus.classList.toggle("none");
+//   minus.classList.toggle("go");
+// });
 
-  //   画像も変更したい
-  plus.classList.toggle("none");
-  minus.classList.toggle("go");
+// questions.forEach((question, index) => {
+//   question.addEventListener("click", () => {
+//     answerAll[index].classList.toggle("open");
+//   });
+// });
+
+// querySelectorAllは配列みたいなの取れてるmapとかが使えるのではないか
+questions.forEach((question, index) => {
+  question.addEventListener("click", () => {
+    answerAll[index].classList.toggle("open");
+    plusAll[index].classList.toggle("none");
+    minusuAll[index].classList.toggle("go");
+  });
 });
 
 // 何個も何個も作るの大変だよ
@@ -51,15 +42,3 @@ question.addEventListener("click", () => {
 // });
 
 // ==========>>>>>===============================
-//クリックしてもコンソールに出なかった！けど、一番上に置いたログはでている。
-// だからファイルのインポートはできているぽい。
-// 次にクリック云々なのか、そもそもanswerとかquestionがどうなっているか、
-// nullでした。クラス名が間違ってるのかな
-// クラス名はコピペしてるから間違ってないぞ。なんだろう
-// 結局１０分くらい考えたり調べたけどわからんくて、GPTに聞いてjsのインポート場所の問題でした。
-// HTMLが読まれる前にJSがあるからまだなんもないよお的な。確蟹
-// 良い感じにlogを使いながらデバッグできていたと思ったのに
-
-//Document の querySelector() メソッドは、指定された CSS セレクターまたは CSS セレクター群に一致する、文書内の最初の要素
-// (Element) を返します。一致するものが見つからない場合は null を返します。
-//２個目作っても反応しなくてなんでかなと思ったけど、最初のやつだけなんだって。
